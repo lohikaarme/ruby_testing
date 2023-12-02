2d3c9146f02b09bb2d5cc7a3670b4f3f6e83e7dd
lohikaarme <stewart.watt@gmail.com>
2023-12-02 08:23:24 -0800

    Complete sections 10

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -89,15 +89,17 @@ end
 describe Drink do
   describe '#initialize' do
     context 'when type is specified and ounces is default' do
+      subject(:my_tea) { described_class.new('tea') }
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(my_tea.type).to eq('tea')
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(my_tea).to be_full
       end
     end
   end
@@ -106,18 +108,20 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+      subject(:my_pop) { described_class.new('pop')}
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(my_pop).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
-
+      subject(:my_milk) { described_class.new('milk',4)}
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(my_milk).to_not be_full
       end
     end
   end
